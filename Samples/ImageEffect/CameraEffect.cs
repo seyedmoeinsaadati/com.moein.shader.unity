150efef4156cf642743436bcfc7fbbbe90d835d7
Seyed Moein Saadati <saadati.moin@gmail.com>
2023-04-20 16:06:40 +0330

    Add Image effects samples

---
+++ Samples/ImageEffect/CameraEffect.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+#if UNITY_EDITOR
+[ExecuteInEditMode]
+#endif
+public class CameraEffect : MonoBehaviour
+{
+
+    public bool autoUpdate;
+    public Material material;
+
+    void Start()
+    {
+        if (null == material || null == material.shader || !material.shader.isSupported)
+        {
+            enabled = false;
+            return;
+        }
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (autoUpdate)
+        {
+            Graphics.Blit(source, destination, material);
+        }        
+    }
+
+}
